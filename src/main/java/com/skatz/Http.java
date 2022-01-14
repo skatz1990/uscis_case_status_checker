357b9ceead3d04596bc2d1562df1c7c8e80f609b
skatz1990 <skatz1990@gmail.com>
2022-01-14 08:28:35 -0500

    finished HTTP class

---
+++ src/main/java/com/skatz/Http.java
@@ -0,0 +1,62 @@
+package com.skatz;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class Http {
+    private final URL url;
+
+    public Http(URL url) {
+        this.url = url;
+    }
+
+    public String postRequest(Map<String, String> parameters) throws Exception {
+        if (parameters == null) {
+            throw new Exception("Parameter map must not be null");
+        }
+
+        String encodedData = URLEncoder.encode(getParamsString(parameters), StandardCharsets.UTF_8);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+
+        conn.setRequestProperty("Content-Length", String.valueOf(encodedData.length()));
+
+        OutputStream os = conn.getOutputStream();
+        os.write(encodedData.getBytes());
+
+        return getResponse(conn);
+    }
+
+    private String getResponse(HttpURLConnection con) throws IOException {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+
+            return content.toString();
+        }
+    }
+
+    private static String getParamsString(Map<String, String> params) {
+        StringBuilder result = new StringBuilder();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            result.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8));
+            result.append("&");
+        }
+
+        String resultString = result.toString();
+        return resultString.length() > 0
+                ? resultString.substring(0, resultString.length() - 1)
+                : resultString;
+    }
+}
--- src/main/java/com/skatz/main.java
+++ src/main/java/com/skatz/main.java
@@ -1,11 +1,26 @@
 package com.skatz;
 
-public class main {
+import java.net.URL;
+import java.util.Map;
+
+public class Main {
+    private static final String url = "https://egov.uscis.gov/casestatus/mycasestatus.do";
+
     public static void main(String[] args) throws Exception {
         if (args.length < 1) {
             throw new Exception("Case status must be provided");
         }
 
-        System.out.println("Success");
+        Map<String, String> parameters = Map.of(
+                "changeLocale", "",
+                "completedActionsCurrentPage", "",
+                "upcomingActionsCurrentPage", "",
+                "appReceiptNum", args[0],
+                "caseStatusSearchBtn", "CHECK+STATUS"
+        );
+
+        Http http = new Http(new URL(url));
+        String response = http.postRequest(parameters);
+        System.out.printf("USCIS response: %n", response);
     }
 }
