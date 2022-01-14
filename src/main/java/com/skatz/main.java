74c897e06b097e8de7e4698b73476e5d4ab3d558
skatz1990 <skatz1990@gmail.com>
2022-01-14 07:25:04 -0500

    main added

---
+++ src/main/java/com/skatz/main.java
@@ -0,0 +1,11 @@
+package com.skatz;
+
+public class main {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            throw new Exception("Case status must be provided");
+        }
+
+        System.out.println("Success");
+    }
+}
